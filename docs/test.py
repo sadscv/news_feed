670c4dd23cf2e3b9e8a04c175b95c9c060a2001e
sadscv <sadscv@hotmail.com>
2017-11-02 23:52:09 +0800

    add demo data: data.txt add doc.md add test.py(temp file for test, to be remove later)

---
+++ docs/test.py
@@ -0,0 +1,7 @@
+with open('data.txt') as f:
+    data = f.readlines()
+    for s in data:
+        list = s.split('html')
+        print(list[0] + 'html')
+        print('#'*30)
+        print(list[1].split('_')[0])
