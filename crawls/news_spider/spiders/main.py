9e396ec0a3aef565255928a8c851c77e5a6b84b7
fangxiang111333 <1229570068@qq.com>
2017-11-20 15:48:29 +0800

    凤凰网,江西晨报,江南都市,新华,央视新闻 (#15)

--- crawls/news_spider/spiders/main.py
+++ crawls/news_spider/spiders/main.py
@@ -1,6 +1,10 @@
 from scrapy import cmdline
 # cmdline.execute("scrapy crawl tencent_news_spider -o tencent.csv".split())
 # cmdline.execute("scrapy crawl netease_news_spider -o netease.csv".split())
-cmdline.execute("scrapy crawl sohu_news_spider -o sohu11_13.csv".split())
+# cmdline.execute("scrapy crawl sohu_news_spider -o sohu11_13.csv".split())
 # cmdline.execute("scrapy crawl sina_news_spider -o sina.csv".split())
 # cmdline.execute("scrapy crawl xinhua_news_spider -o xinhua.csv".split())
+# cmdline.execute("scrapy crawl ifeng_news_spider -o ifeng.csv".split())
+# cmdline.execute("scrapy crawl jndsb_news_spider -o jndsb.csv".split())
+# cmdline.execute("scrapy crawl jxcbw_news_spider -o jxcbw.csv".split())
+cmdline.execute("scrapy crawl cctv_news_spider -o cctv.csv".split())
